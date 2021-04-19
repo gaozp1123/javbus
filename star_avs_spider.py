fb259ad4895ab8544bed81e2a7c166e2467c6022
John gao <1649868243@qq.com>
2021-04-19 13:28:29 +0800

    删除了get_page()函数，将获取页数和获取每页的av连接放在一个函数中完成，修改了程序结构

--- star_avs_spider.py
+++ star_avs_spider.py
@@ -21,7 +21,6 @@ followers = {
     '春菜はな': 'https://www.javbus.com/star/6ya',
     '風間ゆみ': 'https://www.javbus.com/star/2t',
     '白鳥寿美礼': 'https://www.javbus.com/star/7q2',
-    '若月みいな': 'https://www.javbus.com/star/sod',
     'RION': 'https://www.javbus.com/star/p4o',
     '篠田ゆう': 'https://www.javbus.com/star/2pv',
     '吉沢明歩': 'https://www.javbus.com/star/2eg',
@@ -37,7 +36,7 @@ followers = {
     '後藤里香': 'https://www.javbus.com/star/qxw'
 }
 # 保存csv的标题头
-csvheaders = [
+csv_headers = [
     '番号',
     '链接',
     '标题',
@@ -52,37 +51,40 @@ csvheaders = [
     '磁力链接']
 # 存储目前爬取女优所有作品的列表
 star_av_list = []
-# 本地代理设置
+# 本地代理设置，此网站需要代理连接，本人使用的是v2ray
 proxy = {'http': 'http://localhost:10809', 'https': 'http://localhost:10809'}
 # 代理头
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 # ajax请求磁力链接base地址
 baseurl = 'https://www.javbus.com/ajax/uncledatoolsbyajax.php?gid='
 
 
-def get_page(url):
-    # 1.获取当前爬取女优一共有多少页
-    i = 2
-    pages = []
+def process_pages(star_url):
+    # 1.遍历女优所有页，爬取每个页的所有av
+    #   返回女优的av列表和页长
+    print('第1页提取完毕')
+    get_page_items(star_url)
+    page_length = 2
     while True:
-        requesturl = url + '/' + str(i)
-        print(requesturl)
+        request_url = star_url + '/' + str(page_length)
         try:
-            response = requests.get(requesturl, proxies=proxy)
+            response = requests.get(request_url, proxies=proxy, timeout=10)
+            soup = BeautifulSoup(response.text, 'lxml')
+            if soup.title.string == "404 Page Not Found!":
+                page_length -= 1
+                return star_av_list, page_length
+            else:
+                print(f'第{page_length}页提取完毕')
+                get_page_items(request_url)
+                page_length += 1
         except ConnectionError as e:
             print(e.args)
-        soup = BeautifulSoup(response.text, 'lxml')
-        if soup.title.string == "404 Page Not Found!":
-            break
-        else:
-            pages.append(i)
-        i += 1
-    return pages
 
 
 def get_page_items(page_url):
-    # 3.获取当前爬取女友一页有多少av
+    # 2.获取当前爬取女友一页有多少av
     try:
         response = requests.get(url=page_url, headers=headers, proxies=proxy)
         if response.status_code == 200:
@@ -91,61 +93,38 @@ def get_page_items(page_url):
                 response.text)
             for item in items_url:
                 star_av_list.append(item)
-            return star_av_list
+            print(f"一共有{len(star_av_list)}个电影了")
     except ConnectionError as e:
         print(e.args)
 
 
-def process_pages(star_url, pages):
-    # 2.处理每页的内容，将所有页爬取，返回所有页的av
-    get_page_items(star_url)
-    print('第1页提取完毕')
-    for i in range(1, len(pages)):
-        try:
-            request_url = star_url + '/' + str(pages[i])
-        except ConnectionError as e:
-            print(e.args)
-        get_page_items(request_url)
-        print('第{}页提取完毕'.format(i + 1))
-
-
-def get_downloadlink(url, gid, uc, img):
-    # 6.获取一个av的磁力链接
-    # ajax请求头
-    ajaxheaders = {
-        'referer': url,
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36',
-        'x-requested-with': 'XLHttpRequest'
-    }
-    requesturl = baseurl + gid + '&lang=zh&img=' + \
-        img + '&uc=' + uc + '&floor=' + str(46)
+def process_av(star_name, url):
+    #  3.分析一个av的信息
     try:
-        ajaxresponse = requests.get(
-            url=requesturl,
-            headers=ajaxheaders,
-            proxies=proxy)
-        if ajaxresponse.status_code == 200:
-            return ajaxresponse.text
-    except requests.ConnectionError as e:
-        print("error", e.args)
-
-
-def store_av(stroed_file, store_head, data):
-    # 7.保存一个av的数据为csv
-    if not os.path.isfile(stroed_file):
-        is_first_write = 1
-    else:
-        is_first_write = 0
-    with open(stroed_file, 'a', encoding='utf-8-sig', newline='') as f:
-        writer = csv.writer(f)
-        if is_first_write:
-            writer.writerow(store_head)
-        writer.writerow(data)
-    f.close()
+        response = requests.get(url, proxies=proxy)
+    except ConnectionError as e:
+        print(e.args)
+    av_info = get_av_info(url, response)
+    av_info['av_link'] = url
+    store_file = star_name + '.csv'
+    csvdata = [
+        av_info['avid'],
+        av_info['av_link'],
+        av_info['avdesc'],
+        av_info['product_date'],
+        av_info['duartion'],
+        av_info['director'],
+        av_info['producer'],
+        av_info['issuer'],
+        av_info['series'],
+        av_info['Category'],
+        av_info['actors'],
+        av_info['download_link']]
+    store_av(store_file, csv_headers, csvdata)
 
 
 def get_av_info(url, response):
-    # 5.返回av的信息，python字典
+    # 4.返回av的信息，python字典
     av_info = {}
     response.encoding = response.apparent_encoding
     soup = BeautifulSoup(response.text, 'lxml')
@@ -157,8 +136,8 @@ def get_av_info(url, response):
     av_info['avdesc'] = webtitle[len(av_info['avid']) + 1:len(webtitle)]
     av_info['product_date'] = re.search(
         r'\d\d\d\d.\d\d.\d\d', response.text).group(0)
-    if re.search(r'(\d{1,})分鐘', response.text) is not None:
-        av_info['duartion'] = re.search(r'(\d{1,})分鐘', response.text).group(1)
+    if re.search(r'(\d+)分鐘', response.text) is not None:
+        av_info['duartion'] = re.search(r'(\d+)分鐘', response.text).group(1)
     else:
         av_info['duartion'] = ''
 
@@ -203,40 +182,52 @@ def get_av_info(url, response):
     return av_info
 
 
-def process_av(star_name, url):
-    # 4.分析一个av的信息
+def get_downloadlink(url, gid, uc, img):
+    # 5.获取一个av的磁力链接
+    # ajax请求头
+    ajaxheaders = {
+        'referer': url,
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36',
+        'x-requested-with': 'XLHttpRequest'
+    }
+    requesturl = baseurl + gid + '&lang=zh&img=' + \
+        img + '&uc=' + uc + '&floor=' + str(46)
     try:
-        response = requests.get(url, proxies=proxy)
-    except ConnectionError as e:
-        print(e.args)
-    av_info = get_av_info(url, response)
-    av_info['av_link'] = url
-    store_file = star_name + '.csv'
-    csvdata = [
-        av_info['avid'],
-        av_info['av_link'],
-        av_info['avdesc'],
-        av_info['product_date'],
-        av_info['duartion'],
-        av_info['director'],
-        av_info['producer'],
-        av_info['issuer'],
-        av_info['series'],
-        av_info['Category'],
-        av_info['actors'],
-        av_info['download_link']]
-    store_av(store_file, csvheaders, csvdata)
+        ajaxresponse = requests.get(
+            url=requesturl,
+            headers=ajaxheaders,
+            proxies=proxy)
+        if ajaxresponse.status_code == 200:
+            return ajaxresponse.text
+    except requests.ConnectionError as e:
+        print("error", e.args)
+
+
+def store_av(stroed_file, store_head, data):
+    # 6.保存一个av的数据为csv
+    if not os.path.isfile(stroed_file):
+        is_first_write = 1
+    else:
+        is_first_write = 0
+    with open(stroed_file, 'a', encoding='utf-8-sig', newline='') as f:
+        writer = csv.writer(f)
+        if is_first_write:
+            writer.writerow(store_head)
+        writer.writerow(data)
+    f.close()
 
 
 if __name__ == '__main__':
     for name, star_url in followers.items():
-        pages = get_page(star_url)
-        print('{}一共有{}页'.format(name, len(pages)))
-        process_pages(star_url, pages)
-        print(name + "的av有：" + str(len(star_av_list)) + ' 部')
+        aStarAvList, page_length = process_pages(star_url)
+        print(f'{name}一共有{page_length}页')
+        print(name + "的av有：" + str(len(aStarAvList)) + ' 部')
         followers_av = {}
-        for i in range(len(star_av_list)):
-            print(str(i) + '\t', star_av_list[i])
-            process_av(name, star_av_list[i])
-        followers_av['name'] = star_av_list
+        for i in range(len(aStarAvList)):
+            print(f"第{i+1}/{len(aStarAvList)}个" + '\t', aStarAvList[i])
+            process_av(name, aStarAvList[i])
+        followers_av[name] = aStarAvList
+        print(followers_av)
+        # 清空列表，开始下一个
         star_av_list = []
